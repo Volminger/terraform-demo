afbf98e8431aed7f2a1c275dd57393ac4e6e49b7
Volminger <Alexander_volminger@live.se>
2020-05-02 23:17:18 +0200

    public key var added

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -1,15 +1,19 @@
 variable "gcp_project_key" {
-
+  default = ""
 }
 
 variable "gcp_region" {
-
+  default = ""
 }
 
 variable "gcp_zone" {
-
+  default = ""
 }
 
 variable "gcp_account" {
+  default = ""
+}
 
+variable "gcp_public_key" {
+  default = ""
 }
