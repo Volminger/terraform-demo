87bd2bee4584b2e5c4da9e1c2e4791defcfbff95
Volminger <Alexander_volminger@live.se>
2020-05-02 22:40:17 +0200

    First instance on gcp

---
+++ gcp/main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  project = var.gcp_project_key
+  region  = var.gcp_region
+  zone    = var.gcp_account
+  credentials = var.gcp_account
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = google_compute_network.vpc_network.self
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
