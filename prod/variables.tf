801d5aaf1eb591f01a18361bb84f4da9de427cc7
Volminger <Alexander_volminger@live.se>
2020-05-02 16:30:50 +0200

    Now public key is given as variable

--- prod/variables.tf
+++ prod/variables.tf
@@ -1,11 +1,5 @@
-variable "public_key_path" {
-  description = <<DESCRIPTION
-Path to the SSH public key to be used for authentication.
-Ensure this keypair is added to your local SSH agent so provisioners can
-connect.
+variable "public_key" {
 
-Example: ~/.ssh/terraform.pub
-DESCRIPTION
 }
 
 variable "key_name" {
