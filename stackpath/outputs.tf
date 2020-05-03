a84519dd4be9ec2260d6a8e235d6da424c0a02c2
Volminger <Alexander_volminger@live.se>
2020-05-03 18:25:35 +0200

    Fixed outputs

--- stackpath/outputs.tf
+++ stackpath/outputs.tf
@@ -2,7 +2,7 @@ output "traefik-workload-instances" {
   value = {
     for instance in stackpath_compute_workload.traefik-lb.instances :
     instance.name => {
-      "ip_address" = instance.external_ip_address
+      "ip_address" = "${instance.external_ip_address}"
       "phase"      = instance.phase
     }
   }
