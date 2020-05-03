06e4738aa53049e65197ff0003dbd89de745b609
Volminger <Alexander_volminger@live.se>
2020-05-03 11:22:10 +0200

    Instance added to digitalOcean

--- digitalOcean/main.tf
+++ digitalOcean/main.tf
@@ -1,3 +1,30 @@
 provider "digitalocean" {
   token = var.digitalOcean-token
 }
+
+resource "digitalocean_droplet" "www-1" {
+    connection {
+      user = "root"
+      type = "ssh"
+      private_key = var.ssh_private_key
+      timeout = "2m"
+    }
+
+    image = "ubuntu-14-04-x64"
+    name = "www-1"
+    region = "nyc2"
+    size = "512mb"
+    ssh_keys = [
+      var.ssh_public_key
+    ]
+
+    provisioner "remote-exec" {
+      inline = [
+        "export PATH=$PATH:/usr/bin",
+        # install nginx
+        "sudo apt-get update",
+        "sudo apt-get -y install nginx"
+      ]
+    }
+  }
+}
--- digitalOcean/variables.tf
+++ digitalOcean/variables.tf
@@ -1,3 +1,11 @@
 variable "digitalOcean-token" {
   default = ""
 }
+
+variable "ssh_public_key" {
+  default = ""
+}
+
+variable "ssh_private_key" {
+  default = ""
+}
