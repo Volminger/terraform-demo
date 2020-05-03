b4cee553ac98cc3ce3d0454bce30abe3feab9d35
Volminger <Alexander_volminger@live.se>
2020-05-03 14:16:19 +0200

    Basic example for cloudflare instead

--- cloudflare/main.tf
+++ cloudflare/main.tf
@@ -4,29 +4,15 @@ provider "cloudflare" {
   api_key = var.cloudflare_api_key
 }
 
-resource "cloudflare_load_balancer" "bar" {
-  zone_id = "d41d8cd98f00b204e9800998ecf8427e"
-  name = "example-load-balancer"
-  fallback_pool_id = cloudflare_load_balancer_pool.foo.id
-  default_pool_ids = [cloudflare_load_balancer_pool.foo.id]
-  description = "example load balancer using geo-balancing"
-  proxied = true
-  steering_policy = "geo"
-  pop_pools {
-    pop = "LAX"
-    pool_ids = [cloudflare_load_balancer_pool.foo.id]
-  }
-  region_pools {
-    region = "WNAM"
-    pool_ids = [cloudflare_load_balancer_pool.foo.id]
-  }
+
+variable "domain" {
+  default = "example.com"
 }
 
-resource "cloudflare_load_balancer_pool" "foo" {
-  name = "example-lb-pool"
-  origins {
-    name = "example-1"
-    address = "161.35.39.237"
-    enabled = false
-  }
+resource "cloudflare_record" "www" {
+  domain  = "${var.domain}"
+  name    = "www"
+  value   = "203.0.113.10"
+  type    = "A"
+  proxied = true
 }
