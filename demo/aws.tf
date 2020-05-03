a0bc9120a18a2085e0db1a02935b9cd0041ad846
Volminger <Alexander_volminger@live.se>
2020-05-03 22:19:17 +0200

    Aws instance should download html page to display

--- demo/aws.tf
+++ demo/aws.tf
@@ -82,6 +82,8 @@ resource "aws_instance" "terraform_demo" {
       "sudo apt-get -y update",
       "sudo apt-get -y install nginx",
       "sudo service nginx start",
+      cd /var/www/html
+      curl https://github.com/Volminger/terraform-demo/tree/master/demo/aws_instance_index.html -o index.html
     ]
   }
 }
