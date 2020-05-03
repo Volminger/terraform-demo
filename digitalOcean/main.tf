9beb351c4daf855713af9b6c78826f8828084374
Volminger <Alexander_volminger@live.se>
2020-05-03 17:04:12 +0200

    test3

--- digitalOcean/main.tf
+++ digitalOcean/main.tf
@@ -22,47 +22,62 @@ resource "digitalocean_droplet" "www-1" {
     size = "512mb"
     ssh_keys = [digitalocean_ssh_key.demoKey.fingerprint]
 
+
+    user_data     = <<EOT
+#cloud-config
+# update apt on boot
+package_update: true
+# install nginx
+packages:
+- nginx
+write_files:
+- content: |
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>StackPath - Amazon Web Services Instance</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <style>
+      html, body {
+        background: #000;
+        height: 100%;
+        width: 100%;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-flow: column;
+      }
+      img { width: 250px; }
+      svg { padding: 0 40px; }
+      p {
+        color: #fff;
+        font-family: 'Courier New', Courier, monospace;
+        text-align: center;
+        padding: 10px 30px;
+      }
+    </style>
+  </head>
+  <body>
+    <img src="https://www.stackpath.com/content/images/logo-and-branding/stackpath-logo-standard-screen.svg">
+    <p>This request was proxied from <strong>Amazon Web Services</strong></p>
+  </body>
+  </html>
+
+path: /usr/share/app/index.html
+permissions: '0644'
+runcmd:
+- cp /usr/share/app/index.html /usr/share/nginx/html/index.html
+EOT
+
     provisioner "remote-exec" {
       inline = [
         "export PATH=$PATH:/usr/bin",
         # install nginx
         "sudo apt-get update",
-        "sudo apt-get -y install nginx",
+        "sudo apt-get -y install nginx"
       ]
     }
 
-    cat /var/www/html/index.html
-    > <!DOCTYPE html>
-    > <html>
-    > <head>
-    >   <title>StackPath - Google Cloud Platform Instance</title>
-    >   <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    >   <style>
-    >     html, body {
-    >       background: #000;
-    >       height: 100%;
-    >       width: 100%;
-    >       padding: 0;
-    >       margin: 0;
-    >       display: flex;
-    >       justify-content: center;
-    >       align-items: center;
-    >       flex-flow: column;
-    >     }
-    >     img { width: 250px; }
-    >     svg { padding: 0 40px; }
-    >     p {
-    >       color: #fff;
-    >       font-family: 'Courier New', Courier, monospace;
-    >       text-align: center;
-    >       padding: 10px 30px;
-    >     }
-    >   </style>
-    > </head>
-    > <body>
-    >   <img src="https://www.stackpath.com/content/images/logo-and-branding/stackpath-logo-standard-screen.svg">
-    >   <p>This request was proxied from <strong>Google Cloud Platform</strong></p>
-    > </body>
-    > </html>
-    > EOF
 }
