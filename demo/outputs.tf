ea8375bdb7f187e57ea2e66871d170eb616c9cf5
Volminger <Alexander_volminger@live.se>
2020-05-04 12:56:06 +0200

    easter egg added

--- demo/outputs.tf
+++ demo/outputs.tf
@@ -1,3 +1,8 @@
 output "load-balancer-public-ip" {
   value = stackpath_compute_workload.load-balancer.instances[0].external_ip_address
 }
+
+output "lord_of_them_all" {
+  value = "One IaC to rule them all, One IaC to find them,
+One IaC to bring them all and in Terraform bind them."
+}
