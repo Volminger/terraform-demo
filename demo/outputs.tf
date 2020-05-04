520e8d7df22317b2d604e62393cfce15f525782a
Volminger <Alexander_volminger@live.se>
2020-05-04 12:22:04 +0200

    Removed old output

--- demo/outputs.tf
+++ demo/outputs.tf
@@ -1,15 +1,3 @@
-# Output a StackPath compute workload's instances' name, internal IP addresses,
-# and status
-output "my-compute-workload-instances" {
-  value = {
-    for instance in stackpath_compute_workload.load-balancer.instances:
-    instance.name => {
-      ip_address = "${instance.external_ip_address}"
-      phase      = "${instance.phase}"
-    }
-  }
-}
-
 output "load-balancer-public-ip" {
   value = stackpath_compute_workload.load-balancer.instances[0].external_ip_address
 }
