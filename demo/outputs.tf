20b445e3b70015e3a4fd384dfe1e7356923f9bcc
Volminger <Alexander_volminger@live.se>
2020-05-04 14:36:19 +0200

    TEst

--- demo/outputs.tf
+++ demo/outputs.tf
@@ -1,9 +1,20 @@
 output "load-balancer-public-ip" {
-  value = stackpath_compute_workload.load-balancer.instances[0].external_ip_address
+  provisioner "local-exec" {
+  command = "sleep 10"
+}
 
-  depends_on = [stackpath_compute_workload.load-balancer.instances[0]]
+  value = {
+  for instance in stackpath_compute_workload.my-compute-workload.instances:
+  instance.name => {
+    ip_address = instance.external_ip_address
+    phase      = instance.phase
+  }
+}
+
+  depends_on = [stackpath_compute_workload.load-balancer.instances[0].external_ip_address]
 }
 
 output "lord_of_them_all" {
+
   value = "One IaC to rule them all, One IaC to find them, One IaC to bring them all and in Terraform bind them."
 }
