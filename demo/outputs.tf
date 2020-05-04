9d3995105d712add7fa9d9073bf0c6b9e1dfcc80
Volminger <Alexander_volminger@live.se>
2020-05-04 15:30:18 +0200

    Start demo

--- demo/outputs.tf
+++ demo/outputs.tf
@@ -10,6 +10,7 @@ output "load-balancer-public-ip" {
   )
 }
 
+
 output "lord_of_them_all" {
   value = "One IaC to rule them all, One IaC to find them, One IaC to bring them all and in Terraform bind them."
 }
